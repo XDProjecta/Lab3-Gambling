d8f4a9bf8283d6b95a205edefcea7dc1949bab17
Valentina <valentinanino@uninorte.edu.co>
2025-11-19 22:46:01 -0500

    main client modificao

--- main_client.py
+++ main_client.py
@@ -1,9 +1,11 @@
 # main_client.py
 import tkinter as tk
 from client.gui_client import ClientApp
+from client.scenes.menu_scene import setup_menu_styles
 
 def main():
     root = tk.Tk()
+    setup_menu_styles(root)
     app = ClientApp(root)
     root.mainloop()
 
